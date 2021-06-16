08e08dbf15d08582b90a205a0e6cbb3e6c644023
cxan96 <c.p.xanten@gmail.com>
2021-06-16 14:39:51 +0200

    added utility function to run all experiments for a dataset

---
+++ efficient_probit_regression/utils.py
@@ -0,0 +1,33 @@
+from . import settings
+from .datasets import BaseDataset
+from .experiments import LeverageScoreSamplingExperiment, UniformSamplingExperiment
+
+_logger = settings.get_logger()
+
+
+def run_experiments(dataset: BaseDataset, min_size, max_size, step_size, num_runs):
+    # check if results directory exists
+    if not settings.RESULTS_DIR.exists():
+        settings.RESULTS_DIR.mkdir()
+
+    _logger.info("Starting uniform sampling experiment")
+    experiment_uniform = UniformSamplingExperiment(
+        min_size=min_size,
+        max_size=max_size,
+        step_size=step_size,
+        num_runs=num_runs,
+        dataset=dataset,
+        results_filename=settings.RESULTS_DIR / f"{dataset.get_name()}_uniform.csv",
+    )
+    experiment_uniform.run(parallel=True)
+
+    _logger.info("Starting leverage score sampling experiment")
+    experiment_leverage = LeverageScoreSamplingExperiment(
+        min_size=min_size,
+        max_size=max_size,
+        step_size=step_size,
+        num_runs=num_runs,
+        dataset=dataset,
+        results_filename=settings.RESULTS_DIR / f"{dataset.get_name()}_leverage.csv",
+    )
+    experiment_leverage.run(parallel=True)
