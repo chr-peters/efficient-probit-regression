6b44f9ebd276d8ae624d3627019087733e0755ce
cxan96 <c.p.xanten@gmail.com>
2021-06-16 16:05:12 +0200

    added script to run experiments for kddcup

---
+++ scripts/run_experiments_kddcup.py
@@ -0,0 +1,17 @@
+from efficient_probit_regression.datasets import KDDCup
+from efficient_probit_regression.utils import run_experiments
+
+MIN_SIZE = 1000
+MAX_SIZE = 30000
+STEP_SIZE = 1000
+NUM_RUNS = 5
+
+dataset = KDDCup()
+
+run_experiments(
+    dataset=dataset,
+    min_size=MIN_SIZE,
+    max_size=MAX_SIZE,
+    step_size=STEP_SIZE,
+    num_runs=NUM_RUNS,
+)
