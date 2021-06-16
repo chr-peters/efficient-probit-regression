635b67fa5d00490408090f816014de495fbc1e71
cxan96 <c.p.xanten@gmail.com>
2021-06-16 14:42:23 +0200

    added script to run the experiments for the Covertype dataset

---
+++ scripts/run_experiments_covertype.py
@@ -0,0 +1,17 @@
+from efficient_probit_regression.datasets import Covertype
+from efficient_probit_regression.utils import run_experiments
+
+MIN_SIZE = 500
+MAX_SIZE = 15000
+STEP_SIZE = 500
+NUM_RUNS = 5
+
+dataset = Covertype()
+
+run_experiments(
+    dataset=dataset,
+    min_size=MIN_SIZE,
+    max_size=MAX_SIZE,
+    step_size=STEP_SIZE,
+    num_runs=NUM_RUNS,
+)
