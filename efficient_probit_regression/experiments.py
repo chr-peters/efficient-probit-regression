2a55477cd5127f9e62826ba4227edd70aa61c6d9
cxan96 <c.p.xanten@gmail.com>
2021-06-16 14:02:26 +0200

    added experiments

---
+++ efficient_probit_regression/experiments.py
@@ -0,0 +1,203 @@
+import abc
+from time import perf_counter
+
+import numpy as np
+import pandas as pd
+from joblib import Parallel, delayed
+
+from . import settings
+from .datasets import BaseDataset
+from .probit_model import ProbitModel
+from .sampling import leverage_score_sampling, uniform_sampling
+
+_logger = settings.get_logger()
+
+_rng = np.random.default_rng()
+
+
+class BaseExperiment(abc.ABC):
+    def __init__(
+        self,
+        num_runs,
+        min_size,
+        max_size,
+        step_size,
+        dataset: BaseDataset,
+        results_filename,
+    ):
+        self.num_runs = num_runs
+        self.min_size = min_size
+        self.max_size = max_size
+        self.step_size = step_size
+        self.dataset = dataset
+        self.results_filename = results_filename
+
+    @abc.abstractmethod
+    def get_reduced_X_y_weights(self, config):
+        """
+        Abstract method that each experiment overrides to return the reduced
+        matrix X, label vector y and weights that correspond to an experimental config.
+
+        Parameters:
+        -----------
+        config : dict
+            The current experimental config.
+
+        Returns:
+        --------
+        X : np.ndarray
+            Reduced matrix X.
+        y : np.ndarray
+            Reduced label vector y.
+        w : np.ndarray
+            New weight vector.
+        """
+        pass
+
+    def get_config_grid(self):
+        """
+        Returns a list of configurations that are used to run the experiments.
+        """
+        grid = []
+        for size in np.arange(
+            start=self.min_size,
+            stop=self.max_size + self.step_size,
+            step=self.step_size,
+        ):
+            for run in range(1, self.num_runs + 1):
+                grid.append({"run": run, "size": size})
+
+        return grid
+
+    def optimize(self, X, y, w):
+        """
+        Optimize the Probit regression problem given by X, y and w.
+
+        Returns:
+        --------
+        beta_opt : np.ndarray
+            The optimal parameters.
+        """
+        try:
+            model = ProbitModel(X=X, y=y, w=w)
+            model.fit()
+            beta_opt = model.get_params()
+        except ValueError:
+            # this is executet if y only contains 1 or -1 label
+            beta_opt = None
+        return beta_opt
+
+    def run(self, parallel=False, n_jobs=4):
+        """
+        Run the experiment.
+        """
+        X, y = self.dataset.get_X(), self.dataset.get_y()
+        model = ProbitModel(X=X, y=y)
+        model.fit()
+        beta_opt = model.get_params()
+
+        def objective_function(beta):
+            return model.negative_log_likelihood(beta)
+
+        f_opt = objective_function(beta_opt)
+
+        _logger.info("Running experiments...")
+
+        def job_function(cur_config):
+            _logger.info(f"Current experimental config: {cur_config}")
+
+            start_time = perf_counter()
+
+            X_reduced, y_reduced, weights = self.get_reduced_X_y_weights(cur_config)
+            sampling_time = perf_counter() - start_time
+
+            cur_beta_opt = self.optimize(X=X_reduced, y=y_reduced, w=weights)
+            total_time = perf_counter() - start_time
+
+            if cur_beta_opt is not None:
+                cur_ratio = objective_function(cur_beta_opt) / f_opt
+            else:
+                cur_ratio = None
+            return {
+                **cur_config,
+                "ratio": cur_ratio,
+                "sampling_time_s": sampling_time,
+                "total_time_s": total_time,
+            }
+
+        if parallel:
+            results = Parallel(n_jobs=n_jobs)(
+                delayed(job_function)(cur_config)
+                for cur_config in self.get_config_grid()
+            )
+        else:
+            results = [
+                job_function(cur_config) for cur_config in self.get_config_grid()
+            ]
+
+        _logger.info(f"Writing results to {self.results_filename}")
+
+        df = pd.DataFrame(results)
+        df.to_csv(self.results_filename, index=False)
+
+        _logger.info("Done.")
+
+
+class UniformSamplingExperiment(BaseExperiment):
+    def __init__(
+        self,
+        num_runs,
+        min_size,
+        max_size,
+        step_size,
+        dataset: BaseDataset,
+        results_filename,
+    ):
+        super().__init__(
+            num_runs=num_runs,
+            min_size=min_size,
+            max_size=max_size,
+            step_size=step_size,
+            dataset=dataset,
+            results_filename=results_filename,
+        )
+
+    def get_reduced_X_y_weights(self, config):
+        X, y = self.dataset.get_X(), self.dataset.get_y()
+        size = config["size"]
+
+        X_reduced, y_reduced = uniform_sampling(X=X, y=y, sample_size=size)
+
+        weights = np.ones(size)
+
+        return X_reduced, y_reduced, weights
+
+
+class LeverageScoreSamplingExperiment(BaseExperiment):
+    def __init__(
+        self,
+        num_runs,
+        min_size,
+        max_size,
+        step_size,
+        dataset: BaseDataset,
+        results_filename,
+    ):
+        super().__init__(
+            num_runs=num_runs,
+            min_size=min_size,
+            max_size=max_size,
+            step_size=step_size,
+            dataset=dataset,
+            results_filename=results_filename,
+        )
+
+    def get_reduced_X_y_weights(self, config):
+        X, y = self.dataset.get_X(), self.dataset.get_y()
+        size = config["size"]
+
+        X_reduced, y_reduced, weights = leverage_score_sampling(
+            X=X, y=y, sample_size=size, augmented=True
+        )
+
+        return X_reduced, y_reduced, weights
---
+++ tests/test_experiments.py
@@ -0,0 +1,146 @@
+import numpy as np
+import pandas as pd
+from numpy.testing import assert_array_equal
+
+from efficient_probit_regression.datasets import BaseDataset
+from efficient_probit_regression.experiments import (
+    LeverageScoreSamplingExperiment,
+    UniformSamplingExperiment,
+)
+
+
+class ExampleDataset(BaseDataset):
+    def __init__(self):
+        super().__init__(use_caching=False)
+
+    def get_name(self):
+        return "example_name"
+
+    def load_X_y(self):
+        X = np.array([[1, 0], [0.1, 1], [-0.1, 1], [-1, 0], [0, -1]])
+        y = np.array([1, -1, -1, 1, -1])
+        return X, y
+
+
+def test_uniform_sampling_experiment(tmp_path):
+    dataset = ExampleDataset()
+    results_filename = tmp_path / "results.csv"
+    experiment = UniformSamplingExperiment(
+        dataset=dataset,
+        results_filename=results_filename,
+        min_size=1,
+        max_size=5,
+        step_size=2,
+        num_runs=3,
+    )
+    experiment.run()
+
+    df = pd.read_csv(results_filename)
+
+    run_unique, run_counts = np.unique(df["run"], return_counts=True)
+    assert_array_equal(run_unique, [1, 2, 3])
+    assert_array_equal(run_counts, [3, 3, 3])
+
+    assert np.all(df["ratio"][~df["ratio"].isna()] >= 1)
+
+    assert np.sum(df["sampling_time_s"].isna()) == 0
+    assert np.sum(df["total_time_s"].isna()) == 0
+
+    assert np.all(df["sampling_time_s"] > 0)
+    assert np.all(df["total_time_s"] > 0)
+
+
+def test_uniform_sampling_reduction(tmp_path):
+    dataset = ExampleDataset()
+    results_filename = tmp_path / "results.csv"
+    experiment = UniformSamplingExperiment(
+        dataset=dataset,
+        results_filename=results_filename,
+        min_size=1,
+        max_size=5,
+        step_size=1,
+        num_runs=1,
+    )
+
+    for cur_config in experiment.get_config_grid():
+        cur_X, cur_y, cur_weights = experiment.get_reduced_X_y_weights(cur_config)
+        assert_array_equal(cur_weights, np.ones(cur_config["size"]))
+        assert cur_X.shape[0] == cur_config["size"]
+        assert cur_X.shape[1] == dataset.get_d()
+        assert cur_y.shape[0] == cur_config["size"]
+
+
+def test_leverage_score_sampling_experiment(tmp_path):
+    dataset = ExampleDataset()
+    results_filename = tmp_path / "results.csv"
+    experiment = LeverageScoreSamplingExperiment(
+        dataset=dataset,
+        results_filename=results_filename,
+        min_size=1,
+        max_size=5,
+        step_size=2,
+        num_runs=3,
+    )
+    experiment.run()
+
+    df = pd.read_csv(results_filename)
+
+    run_unique, run_counts = np.unique(df["run"], return_counts=True)
+    assert_array_equal(run_unique, [1, 2, 3])
+    assert_array_equal(run_counts, [3, 3, 3])
+
+    assert np.all(df["ratio"][~df["ratio"].isna()] >= 1)
+
+    assert np.sum(df["sampling_time_s"].isna()) == 0
+    assert np.sum(df["total_time_s"].isna()) == 0
+
+    assert np.all(df["sampling_time_s"] > 0)
+    assert np.all(df["total_time_s"] > 0)
+
+
+def test_leverage_score_sampling_experiment_parallel(tmp_path):
+    dataset = ExampleDataset()
+    results_filename = tmp_path / "results.csv"
+    experiment = LeverageScoreSamplingExperiment(
+        dataset=dataset,
+        results_filename=results_filename,
+        min_size=1,
+        max_size=5,
+        step_size=2,
+        num_runs=3,
+    )
+    experiment.run(parallel=True)
+
+    df = pd.read_csv(results_filename)
+
+    run_unique, run_counts = np.unique(df["run"], return_counts=True)
+    assert_array_equal(run_unique, [1, 2, 3])
+    assert_array_equal(run_counts, [3, 3, 3])
+
+    assert np.all(df["ratio"][~df["ratio"].isna()] >= 1)
+
+    assert np.sum(df["sampling_time_s"].isna()) == 0
+    assert np.sum(df["total_time_s"].isna()) == 0
+
+    assert np.all(df["sampling_time_s"] > 0)
+    assert np.all(df["total_time_s"] > 0)
+
+
+def test_leverage_score_sampling_reduction(tmp_path):
+    dataset = ExampleDataset()
+    results_filename = tmp_path / "results.csv"
+    experiment = LeverageScoreSamplingExperiment(
+        dataset=dataset,
+        results_filename=results_filename,
+        min_size=1,
+        max_size=5,
+        step_size=1,
+        num_runs=1,
+    )
+
+    for cur_config in experiment.get_config_grid():
+        cur_X, cur_y, cur_weights = experiment.get_reduced_X_y_weights(cur_config)
+        assert cur_X.shape[0] == cur_config["size"]
+        assert cur_y.shape[0] == cur_config["size"]
+        assert cur_X.shape[1] == dataset.get_d()
+        assert cur_weights.shape[0] == cur_config["size"]
