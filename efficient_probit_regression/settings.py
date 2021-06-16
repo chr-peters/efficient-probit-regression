a451589cb5fe117ddc78ca1fc5049d3790e6d62a
cxan96 <c.p.xanten@gmail.com>
2021-06-16 12:02:48 +0200

    logger is configured in settings module

--- efficient_probit_regression/settings.py
+++ efficient_probit_regression/settings.py
@@ -1,3 +1,4 @@
+import logging
 from pathlib import Path
 
 BASE_DIR = Path(__file__).parent.parent
@@ -9,4 +10,27 @@ RESULTS_DIR = BASE_DIR / "experimental-results"
 
 PLOTS_DIR = BASE_DIR / "plots"
 
-LOGGER_NAME = "efficient_probit_regression"
+
+_logger = None
+
+
+def get_logger():
+    global _logger
+
+    if _logger is not None:
+        return _logger
+
+    _logger = logging.getLogger("efficient-probit-regression")
+    _logger.setLevel(logging.DEBUG)
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(logging.DEBUG)
+    stream_handler.setFormatter(
+        logging.Formatter(
+            "%(asctime)s - PID: %(process)d - "
+            "PName: %(processName)s - %(levelname)s - %(message)s"
+        )
+    )
+    _logger.addHandler(stream_handler)
+
+    return _logger
