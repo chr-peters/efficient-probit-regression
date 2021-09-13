cef41e350d878c04bf4a3437fe8753e130d2d068
cxan96 <c.p.xanten@gmail.com>
2021-09-13 16:12:12 +0200

    rewrote mmd, added polynomial kernel

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -1,10 +1,12 @@
 import numpy as np
+import pytest
 from scipy.stats import multivariate_normal
 
-from efficient_probit_regression.metrics import mmd
+from efficient_probit_regression.metrics import gaussian_kernel, mmd, polynomial_kernel
 
 
-def test_mmd():
+@pytest.mark.parametrize("kernel_function", [gaussian_kernel, polynomial_kernel])
+def test_mmd(kernel_function):
     seed = 1
 
     # equal distributions
@@ -15,7 +17,8 @@ def test_mmd():
         mean=np.array([1, 2, 3]), cov=np.eye(3), size=1000, random_state=seed
     )
 
-    mmd_value = mmd(sample_1, sample_2)
+    mmd_value = mmd(sample_1, sample_2, kernel_function=kernel_function)
+    print(mmd_value)
 
     assert mmd_value <= 0.0001
 
@@ -27,7 +30,8 @@ def test_mmd():
         mean=np.array([2, 2, 3]), cov=np.eye(3), size=1000, random_state=seed
     )
 
-    mmd_value = mmd(sample_1, sample_2)
+    mmd_value = mmd(sample_1, sample_2, kernel_function=kernel_function)
+    print(mmd_value)
 
     assert mmd_value > 0.1
 
@@ -39,6 +43,7 @@ def test_mmd():
         mean=np.array([1, 2, 3]), cov=2 * np.eye(3), size=1000, random_state=seed
     )
 
-    mmd_value = mmd(sample_1, sample_2)
+    mmd_value = mmd(sample_1, sample_2, kernel_function=kernel_function)
+    print(mmd_value)
 
     assert mmd_value > 0.1
