c16b46d58f83e311eaa29afb3c68f8d5457515d4
cxan96 <c.p.xanten@gmail.com>
2021-10-14 21:21:07 +0200

    updated scripts for running the bayesian experiments

--- scripts/run_bayes_experiments_leverage.py
+++ scripts/run_bayes_experiments_leverage.py
@@ -1,20 +1,18 @@
 import numpy as np
 
-from efficient_probit_regression.datasets import Covertype
+from efficient_probit_regression.datasets import Covertype, KDDCup, Webspam  # noqa
 from efficient_probit_regression.experiments import LeverageScoreSamplingExperimentBayes
 from efficient_probit_regression.settings import get_logger
 
 logger = get_logger()
 
-min_size = 500
-max_size = 15000
-step_size = 500
-
 num_runs = 5
 samples_per_chain = 1000
 num_chains = 1
 
-dataset = Covertype()
+# dataset = Covertype()
+# dataset = KDDCup()
+dataset = Webspam()
 
 prior_mean = np.zeros(dataset.get_d())
 prior_cov = 10 * np.eye(dataset.get_d())
@@ -23,10 +21,29 @@ if dataset.get_name() == "covertype":
     burn_in = 100
 elif dataset.get_name() == "kddcup":
     burn_in = 2000
+elif dataset.get_name() == "webspam":
+    burn_in = 3000
 else:
     raise ValueError("Unknown dataset! Can't determine burn_in!")
 
 logger.info(f"Setting burn_in = {burn_in}")
+
+if dataset.get_name() in ["covertype", "webspam"]:
+    min_size = 500
+    max_size = 15000
+    step_size = 500
+elif dataset.get_name() == "kddcup":
+    min_size = 1000
+    max_size = 30000
+    step_size = 1000
+else:
+    raise ValueError(
+        "Unknown dataset! Can't determine min_size, max_size and step_size!"
+    )
+
+logger.info(
+    f"Settings min_size = {min_size}, max_size = {max_size} and step_size = {step_size}"
+)
 
 experiment = LeverageScoreSamplingExperimentBayes(
     dataset=dataset,
