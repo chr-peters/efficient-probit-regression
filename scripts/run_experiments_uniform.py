ddd7c62218a67e93df8ee81bf1fb6c0f62fb639a
cxan96 <c.p.xanten@gmail.com>
2021-10-17 14:14:31 +0200

    updated uniform sampling script

--- scripts/run_experiments_uniform.py
+++ scripts/run_experiments_uniform.py
@@ -2,31 +2,39 @@ from efficient_probit_regression import settings
 from efficient_probit_regression.datasets import Covertype, KDDCup, Webspam  # noqa
 from efficient_probit_regression.experiments import UniformSamplingExperiment
 
-_logger = settings.get_logger()
+logger = settings.get_logger()
 
-MIN_SIZE = 500
-MAX_SIZE = 15000
-STEP_SIZE = 500
-NUM_RUNS = 21
-
-# MIN_SIZE = 1000
-# MAX_SIZE = 30000
-# STEP_SIZE = 1000
-# NUM_RUNS = 21
-
-P = 1
+num_runs = 30
+P = 2
 
 dataset = Covertype()
 # dataset = KDDCup()
 # dataset = Webspam()
 
-_logger.info("Starting uniform sampling experiment")
+if dataset.get_name() in ["covertype", "webspam"]:
+    min_size = 500
+    max_size = 15000
+    step_size = 500
+elif dataset.get_name() == "kddcup":
+    min_size = 1000
+    max_size = 30000
+    step_size = 1000
+else:
+    raise ValueError(
+        "Unknown dataset! Can't determine min_size, max_size and step_size!"
+    )
+
+logger.info(
+    f"Settings min_size = {min_size}, max_size = {max_size} and step_size = {step_size}"
+)
+
+logger.info("Starting uniform sampling experiment")
 experiment = UniformSamplingExperiment(
     p=P,
-    min_size=MIN_SIZE,
-    max_size=MAX_SIZE,
-    step_size=STEP_SIZE,
-    num_runs=NUM_RUNS,
+    min_size=min_size,
+    max_size=max_size,
+    step_size=step_size,
+    num_runs=num_runs,
     dataset=dataset,
     results_filename=settings.get_results_dir_p(P)
     / f"{dataset.get_name()}_uniform_p_{P}.csv",
