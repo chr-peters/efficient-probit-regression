20fbe0d2945b394b7d504322a4ee52ee6816b22e
SamuelToporovskij <samuel.toporovskij@tu-dortmund.de>
2021-12-08 11:47:39 +0100

    Test: get Iris data with data.get_X()

--- scripts/Test.py
+++ scripts/Test.py
@@ -1,4 +1,8 @@
-x = 2
-if x == 2: 
-    print("Hello World")
-    
+from efficient_probit_regression.datasets import Iris 
+
+# Iris() ist ein objekt, daher die Klammern
+data = Iris()
+
+# die Funktion get_X() erzeugt aus dem Datensatz Iris() einen DataFrame X
+# returned numpy.array mit Daten von X
+print(data.get_X())
